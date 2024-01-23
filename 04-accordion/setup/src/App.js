c194888c06aa28a0e93fa8b7630bfdff2274200c
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-01-23 15:40:51 -0600

    complete exercise 04

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+
+  //in app iterate over list of questions in data
+  //for every item display the single question
+  //toggle display text or hide text
 }
 
 export default App;
